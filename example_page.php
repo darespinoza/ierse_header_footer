38026ee680e748a20daa519734bfd78775d5d00b
darespinoza <despinoza@uazuay.edu.ec>
2024-02-21 17:27:38 -0500

    Correct styles for uda services links

--- example_page.php
+++ example_page.php
@@ -11,7 +11,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Bootstrap Icons   -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-        <!-- jQuery 3.5.1   -->
+        <!-- jQuery 3.6   -->
         <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
         <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
 
